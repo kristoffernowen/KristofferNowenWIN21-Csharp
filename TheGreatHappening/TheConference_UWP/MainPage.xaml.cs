11031a5d85bf5571cba5a7c88010e66c26c2a231
Kristoffer Nowen <kristoffer.nowen@utb.ecutbildning.se>
2021-12-14 19:06:33 +0100

    tisdag

---
+++ TheGreatHappening/TheConference_UWP/MainPage.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using TheConference_UWP.Services;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace TheConference_UWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        List<UwpParticipant> listOfUwpParticipants = new List<UwpParticipant>();
+        string name = "Vivo";
+        public MainPage()
+        {
+            this.InitializeComponent();
+            
+        }
+
+        private void StateTimer()
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(500);
+            timer.Tick += TickAction;
+            timer.Start();
+        }
+
+        public void TickAction(object sender, object e)
+        {
+            lvListOfParticipants.ItemsSource = listOfUwpParticipants;
+        }
+        private void btnSaveParticipantToList_Click(object sender, RoutedEventArgs e)
+        {
+
+            listOfUwpParticipants.Add(UwpParticipant.CreateParticipant(tboxFirstName.Text, tboxLastName.Text, tboxEmail.Text, tboxSpecialRequirements.Text));
+            
+        }
+
+        private void btnShowList_Click(object sender, RoutedEventArgs e)
+        {
+            List<UwpParticipant> nextList = new List<UwpParticipant>();
+            nextList = listOfUwpParticipants;
+
+            IEnumerable<UwpParticipant> listU = listOfUwpParticipants as IEnumerable<UwpParticipant>;
+            
+            lvListOfParticipants.ItemsSource = nextList;
+
+            name = "måns";
+            
+        }
+
+        private void btnClearList_Click(object sender, RoutedEventArgs e)
+        {
+            listOfUwpParticipants.Clear();
+            lvListOfParticipants.ItemsSource = listOfUwpParticipants;
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            IEnumerable<UwpParticipant> listU = listOfUwpParticipants as IEnumerable<UwpParticipant>;
+            var obj =(Button)sender;
+            var item =(UwpParticipant)obj.DataContext;
+            
+
+            listOfUwpParticipants = listU.Where(participant => participant != item).ToList();
+            
+            // kan inte uppdatera listvy här, då låser jag den för showlist(), är det därför jag ska ha en uppdateande klocka?   is funky...
+            name = "Fran";
+            tblockTest.Text = name;
+        }
+    }
+}
---
+++ TheGreatHappening/TheConference_UWP/Services/UwpParticipant.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheConference_UWP.Services
+{
+    internal class UwpParticipant
+    {
+        Random random = new Random();
+
+
+        public UwpParticipant()
+        {
+            var dateHere = DateTime.Now;
+            DiscountCode = $"TGH{dateHere.Year}{random.Next(0, 100)}";
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+        public string SpecialRequirements { get; set; }
+        public string DiscountCode { get; }
+        public string FullName => $"{FirstName} {LastName }";
+
+        public static UwpParticipant CreateParticipant(string firstName, string lastName, string email, string specialRequirements)
+        {
+            var newParticipant = new UwpParticipant();
+            newParticipant.FirstName = firstName;
+            newParticipant.LastName = lastName;
+            newParticipant.Email = email;
+            newParticipant.SpecialRequirements = specialRequirements;
+
+            return newParticipant;
+        }
+
+        public static List<UwpParticipant> ShowList(List<UwpParticipant> list)
+        {
+            return list;
+
+        }
+
+        
+    }
+}
